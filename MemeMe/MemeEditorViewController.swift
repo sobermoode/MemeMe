1063f59d1dbca2d1afc3da5748719bb7ed9b6b43
Aaron <J>
2015-03-30 22:28:14 -0700

    Added text field style attributes.

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,7 +8,44 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
+class MemeEditorViewController: UIViewController
+{
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    // options for meme text
+    let textFieldAttributes =
+    [
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont( name: "HelveticaNeue-CondensedBlack", size: 28 )!,
+        NSStrokeWidthAttributeName: -4.0,
+    ]
+    
+    // flag and vars to retain inputted meme text if the user
+    // brings up the image picker and then cancels
+    var oldTopText: String?
+    var oldBottomText: String?
+    var didCancel: Bool = false
+    
+    override func viewWillAppear( animated: Bool )
+    {
+        // set the appearance of the text fields' text
+        setTextFields()
+    }
+    
+    func setTextFields()
+    {
+        topText.defaultTextAttributes = textFieldAttributes
+        topText.borderStyle = UITextBorderStyle.None
+        topText.textAlignment = NSTextAlignment.Center
+        topText.text = ( didCancel ) ? oldTopText : ""
+        
+        bottomText.defaultTextAttributes = textFieldAttributes
+        bottomText.borderStyle = UITextBorderStyle.None
+        bottomText.textAlignment = NSTextAlignment.Center
+        bottomText.text = ( didCancel ) ? oldBottomText : ""
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
